b735793b19a1970281e4963ef88789b1a0ea7b77
Anthony Garcia <anthonyjaemes98@gmail.com>
2023-12-28 21:16:22 -0800

    Added sorting functionality

--- To-Do-App/Controllers/ToDoController.cs
+++ To-Do-App/Controllers/ToDoController.cs
@@ -12,9 +12,54 @@ namespace To_Do_App.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string sortOrder)
         {
-            List<TaskModel> objTaskList = _db.Tasks.ToList();
+            ViewData["IdSortParam"] = string.IsNullOrEmpty(sortOrder) || sortOrder != "id_desc" ? "id_desc" : "id";
+            ViewData["CategorySortParam"] = sortOrder == "category" ? "category_desc" : "category";
+            ViewData["TaskNameSortParam"] = sortOrder == "taskName" ? "taskName_desc" : "taskName";
+            ViewData["PrioritySortParam"] = sortOrder == "priority" ? "priority_desc" : "priority";
+            ViewData["DueDateSortParam"] = sortOrder == "dueDate" ? "dueDate_desc" : "dueDate";
+
+            IQueryable<TaskModel> tasks = _db.Tasks.AsQueryable();
+
+            switch (sortOrder)
+            {
+                case "id":
+                    tasks = tasks.OrderBy(task => task.Id);
+                    break;
+                case "id_desc":
+                    tasks = tasks.OrderByDescending(task => task.Id);
+                    break;
+                case "category":
+                    tasks = tasks.OrderBy(task => task.category);
+                    break;
+                case "category_desc":
+                    tasks = tasks.OrderByDescending(task => task.category);
+                    break;
+                case "taskName":
+                    tasks = tasks.OrderBy(task => task.taskName);
+                    break;
+                case "taskName_desc":
+                    tasks = tasks.OrderByDescending(task => task.taskName);
+                    break;
+                case "priority":
+                    tasks = tasks.OrderBy(task => task.priorityLevel);
+                    break;
+                case "priority_desc":
+                    tasks = tasks.OrderByDescending(task => task.priorityLevel);
+                    break;
+                case "dueDate":
+                    tasks = tasks.OrderBy(task => task.dueDate);
+                    break;
+                case "dueDate_desc":
+                    tasks = tasks.OrderByDescending(task => task.dueDate);
+                    break;
+                default:
+                    tasks = tasks.OrderByDescending(task => task.Id);
+                    break;
+            }
+
+            List<TaskModel> objTaskList = tasks.ToList();
             return View(objTaskList);
         }
 
